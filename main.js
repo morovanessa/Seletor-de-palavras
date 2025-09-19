2118621e09e7d2455d0c08cedbc79cd01793bc98
Vanessa Moro <vanessa.aparecida.moro@escola.pr.gov.br>
2025-09-19 08:20:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,5 +12,46 @@ function mostraPalavrasChave(){
 
 function processaTexto(texto){
   let palavras = texto.split(/\P{L}+/u);
-  return palavras;
+  
+  for(let i in palavras){
+    palavras[i] = palavras[i].toLowerCase();
+  }
+ 
+  palavras = tiraPalavrasRuins(palavras);
+
+const frequencias = contarFrequencias(palavras);
+let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+function ordenaPalavra(p1,p2){
+   return frequencias [p2] - frequencias[p1];
+
+}
+
+console.log(ordenadas);
+   return ordenadas.slice(0,10);
+
+}
+
+function contaFrequencias(palavras){
+  let frequencias = {};
+  for (let i of palavras){
+    frequencias[i] = 0;
+    for(let j of palavras){
+      if (i==j){
+        frequencias[i]++;
+      }
+    }
+  }
+  return frequencias 
+}
+
+function tiraPalavrasRuins(palavras){
+  const PALAVRAS_RUINS = new Set(["para","uma","nós"]);
+  const palavrasBoas = [];
+  for(let palavra of palavras){
+    if(!PALAVRAS_RUINS.has(palavra)&& palavra.lenght > 2){
+      palavrasBoas.push(palavra);
+    }
+  }
+  return palavrasBoas;
 }
